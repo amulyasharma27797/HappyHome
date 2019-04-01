eb0975635abc800a37e8f4f4ae12fa6c8177c3c4
Amulya Sharma <amulya.sharma@tothenew.com>
2019-04-01 22:12:39 +0530

    Added user model

--- happyhome/homeapp/models.py
+++ happyhome/homeapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    image = models.ImageField(blank=True)
+    user_name = models.CharField(max_length=20)
+    email = models.EmailField()
+    phone_number = models.IntegerField()
